ab662ca00da34f04842c9dd5e72a71c57d953056
Onishida001 <samuelmagnobsf@gmail.com>
2025-10-05 20:56:58 -0300

    Update Questão 1.cpp

--- Questão 1.cpp
+++ Questão 1.cpp
@@ -5,18 +5,34 @@
 #include <stdlib.h>
 
 int main() {
-    int n1, n2, sm;
+    int n1, n2;
+    int s1 = n1;
+    int a1 = n1;
+    int s2 = n2;
+    int a2 = n2;
 
-    printf("Digite o primeiro numero: ");
+    printf("Digite o primeiro numero inteiro: ");
     scanf("%i", &n1);
-    
-    printf("Digite o segundo numero: ");
+
+    printf("Digite o segundo numero inteiro: ");
     scanf("%i", &n2);
     
-    sm == n1 + n2;
-    
-    printf("A soma dos dois numeros digitados e: %i", sm);
-    
+    s1++;
+    a1--;
+    s2++;
+    a2--;
+
+    printf("\n--- Resultados ---\n");
+    printf("Numero digitado: %i\n", n1);
+    printf("Seu antecessor: %i\n", a1);
+    printf("Seu sucessor: %i\n", s1);
+    printf("\n");
+    printf("Numero digitado: %i\n", n2);
+    printf("Seu antecessor: %i\n", a2);
+    printf("Seu sucessor: %i\n", s2);
+    printf("\n");
+
+    system("pause");
 
     return 0;
 }
